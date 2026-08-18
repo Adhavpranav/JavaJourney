c9e10bab2468495faa8e894ad9d67610bf1bb171
Pranav Adhav <142861984+Adhavpranav@users.noreply.github.com>
2026-08-18 16:44:44 +0530

    Add BasicInheritance class with Animal and Dog classes

---
+++ CoreJava/BasicInheritance.java
@@ -0,0 +1,22 @@
+package CoreJava;
+import  java.util.*;
+
+class Animal{
+    protected void eat(){
+        System.out.println("Animal is eating");
+    }
+}
+
+class Dog extends Animal{
+    protected void bark(){
+        System.out.println("Dog is barking");
+    }
+}
+
+public class BasicInheritance{
+    public static void main(String[] args) {
+        Dog obj=new Dog();
+        obj.eat();
+        obj.bark();
+    }
+}
